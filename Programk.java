67fae4db4dcd15acb6ec01b4afed9d16726d9f94
Prakashkumar2001 <prakashvalarmathi2001@gmail.com>
2023-12-29 13:37:20 +0530

    star program

---
+++ Programk.java
@@ -0,0 +1,17 @@
+class Programk{
+
+public static void main(String arg[]){
+
+  for(int i=0;i<=7;i++){
+    for(int j=0;j<=i;j++){
+
+          System.out.print("*");
+
+    }
+   
+    System.out.println();
+  }
+
+}
+
+}
